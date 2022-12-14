d7aa7e59bd94d5f83dda7cf36b0d59e7d2fcf10e
VLAD <Cumpatescuvlad@yaho.com>
2022-12-14 03:36:11 +0200

    Version V0.5

--- PricesApi/AdsScrapper.cs
+++ PricesApi/AdsScrapper.cs
@@ -1,6 +1,8 @@
 ﻿using HtmlAgilityPack;
 using System.Net.Http;
 using System.Linq;
+using System.Diagnostics.Contracts;
+
 namespace DataScrapper
 {
     public class AdsScrapper
@@ -10,9 +12,9 @@ namespace DataScrapper
        
         public string EmagResults(string searchitem)
         {
-            var html = client.GetStringAsync($"https://www.emag.ro/search/{searchitem}").Result;
+            var htmlPage = client.GetStringAsync($"https://www.emag.ro/search/{searchitem}").Result;
 
-            document.LoadHtml(html);
+            document.LoadHtml(htmlPage);
 
             var adsTitles = document.DocumentNode.SelectNodes("//div[@class='pad-hrz-xs']/a[@data-zone='title']");
 
@@ -36,31 +38,168 @@ namespace DataScrapper
             return $"{AdSpecs.AdPrice}{AdSpecs.AdHyperlink}{AdSpecs.AdTitle}";
 
         }
+        #region !Working
 
-        public string EvomagResults(string searchItem)
+        //public string EvomagResults(string searchItem)
+        //{
+        //    var htmlPage = client.GetStringAsync($"https://www.evomag.ro/?sn.q={searchItem}").Result;
+
+        //    document.LoadHtml(htmlPage);
+
+        //    //var adsTitles = document.DocumentNode.SelectNodes("//div[@class='npi_name']/a");
+
+        //    var adsTitles = document.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("npi_name")).ToList();
+
+        //    foreach (var adTitle in adsTitles)
+        //    {
+        //        // var aElement = adTitle.SelectSingleNode("a");
+        //        //AdSpecs.AdHyperlink += $"{aElement.Attributes["href"].Value}\n";
+        //        //AdSpecs.AdTitle += $"{aElement.InnerText}\n";
+
+        //        var variable = adTitle.SelectSingleNode("//a");
+
+        //        AdSpecs.AdTitle += $"{variable.InnerText}";
+
+        //        AdSpecs.AdHyperlink += $"{variable.Attributes["href"].Value}";
+        //    }
+
+        //    //var adsPrices = document.DocumentNode.SelectNodes("//span[@class='real_price']");
+
+        //    //foreach (var adPrice in adsPrices)
+        //    //{
+        //    //    AdSpecs.AdPrice += $"{adPrice.InnerText}";
+        //    //}
+
+        //    return $"{AdSpecs.AdHyperlink}{AdSpecs.AdTitle}";
+        //}
+
+        //public string ITArenaResults(string searchItem)
+        //{
+        //    var htmlPage = client.GetStringAsync($"https://www.itarena.ro/?sn.q={searchItem}").Result;
+
+        //    document.LoadHtml(htmlPage);
+
+        //    var adsTitles = document.DocumentNode.Descendants("h4").Where(node=>node.GetAttributeValue("class","").Equals("product-title")).ToList();
+
+        //    foreach (var adTitle in adsTitles) 
+        //    {
+        //        var title = adTitle.Descendants("a").FirstOrDefault();
+
+        //        AdSpecs.AdTitle += $"{title.InnerText}\n";
+
+        //    }  
+
+        //    return $"{AdSpecs.AdTitle}";
+        //}
+
+        //public string IPonResults(string searchItem)
+        //{
+        //    var htmlPage = client.GetStringAsync($"https://ipon.ro/cautare/shop?keyword={searchItem}").Result;
+
+        //    document.LoadHtml(htmlPage);
+
+        //    var adsTitles = document.DocumentNode.SelectNodes("//a[@class='shop-card__title']");
+
+        //    foreach (var adTitle in adsTitles)
+        //    {
+
+        //        AdSpecs.AdHyperlink += $"{adTitle.InnerText.Trim()}\n";
+
+        //    }
+
+        //    return $"{AdSpecs.AdTitle}{AdSpecs.AdHyperlink}";
+        //}
+
+
+        #endregion
+
+        public string ForITResults(string searchItem) 
         {
-            var html = client.GetStringAsync($"https://www.evomag.ro/?sn.q={searchItem}").Result;
+            var htmlPage = client.GetStringAsync($"https://www.forit.ro/search/?c=0&q={searchItem}").Result;
 
-            document.LoadHtml(html);
+            document.LoadHtml(htmlPage);
 
-            var adsTitles = document.DocumentNode.SelectNodes("//div[@class='npi_name']/a[not(@id) and not(@class)]");
+            var adsTitles = document.DocumentNode.SelectNodes("//h5[@class='name']/a[@data-ecproduct='true']");
 
-            foreach (var adtitle in adsTitles)
+            foreach (var adTitle in adsTitles)
             {
-                AdSpecs.AdHyperlink += $"{adtitle.Attributes["href"].Value.Trim()}";
-                AdSpecs.AdTitle += $"{adtitle.InnerText.Trim()}";
+                AdSpecs.AdTitle += $"{adTitle.InnerText.Trim()}\n";
+
+                AdSpecs.AdHyperlink += $"{adTitle.Attributes["href"].Value.Trim()}\n";
+
             }
 
-            var adsPrices = document.DocumentNode.SelectNodes(".span[@class='real_price']");
+            var adPrices = document.DocumentNode.SelectNodes("//div[@class='price-value text-bold']");
+
+            foreach (var adPrice in adPrices)
+            {
+                AdSpecs.AdPrice += $"{adPrice.InnerText.Trim()}\n";
+            }
+
+            return $"{AdSpecs.AdTitle}{AdSpecs.AdPrice}{AdSpecs.AdHyperlink}";
+        
+        }
+
+        public string PcOneResults(string searchItem) 
+        {
+            var htmlPage = client.GetStringAsync($"https://www.pcone.ro/search/?q={searchItem}").Result;
+
+            document.LoadHtml(htmlPage);
+
+            var adsTitles = document.DocumentNode.SelectNodes("//h2[@class='name']/a[@data-ecproduct='true']");
+
+            foreach (var adTitle in adsTitles)
+            {
+                AdSpecs.AdTitle += $"{adTitle.Attributes["title"].Value.Trim()}\n";
+                AdSpecs.AdHyperlink += $"{adTitle.Attributes["href"].Value.Trim()}\n";
+
+            }
+
+            var adsPrices = document.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("pull-left")).ToList();
 
             foreach (var adPrice in adsPrices)
             {
-                AdSpecs.AdPrice += $"{adPrice.InnerText}";
+                AdSpecs.AdPrice += $"{adPrice.InnerText}\n";
             }
 
-            return $"{AdSpecs.AdHyperlink}{AdSpecs.AdTitle}{AdSpecs.AdPrice}";
+            return $"{AdSpecs.AdTitle}{AdSpecs.AdHyperlink}{AdSpecs.AdPrice}";
+        }
+        
+        public string VexioResults(string searchItem)
+        {
+            var htmlPage = client.GetStringAsync($"https://www.flax.ro/produse?term={searchItem}").Result;
+
+            document.LoadHtml(htmlPage);
+
+            var adsTitles = document.DocumentNode.SelectNodes("//h2[@class='product-title']/a[@data-ecproduct='true']");
+
+            foreach (var adTitle in adsTitles) 
+            {
+                AdSpecs.AdTitle += $"{adTitle.Attributes["title"].Value.Trim()}\n";
+                AdSpecs.AdHyperlink += $"{adTitle.Attributes["href"].Value.Trim()}\n";
+            }
+
+            return $"{AdSpecs.AdTitle}{AdSpecs.AdHyperlink}";
         }
 
+        public string ItGalaxyResults(string searchItem) 
+        {
+            var htmlPage = client.GetStringAsync($"https://www.itgalaxy.ro/cauta/{searchItem}").Result;
+
+            document.LoadHtml(htmlPage);
+
+            var adsTitles = document.DocumentNode.SelectNodes("//h5[@class='name']/a[@data-ecproduct='true']");
+
+            foreach (var adTitle in adsTitles)
+            {
+                AdSpecs.AdTitle += $"{adTitle.Attributes["title"].Value.Trim()}\n";
+                AdSpecs.AdHyperlink += $"{adTitle.Attributes["href"].Value.Trim()}\n";
+            }
+
+            return $"{AdSpecs.AdTitle}{AdSpecs.AdHyperlink}";
+        }
+
+       
     }
 }
 
--- PricesApi/Controllers/EmagData.cs
+++ PricesApi/Controllers/EmagData.cs
@@ -5,7 +5,7 @@ namespace DataScrapper
     [ApiController]
     public class EmagData : ControllerBase
     {
-        AdsScrapper _scapper = new AdsScrapper();
+        private readonly AdsScrapper _scapper = new();
 
         [Route("api/EmagResults/{searchItem}")]
 
@@ -13,10 +13,50 @@ namespace DataScrapper
 
         public string ReadEmagAds(string searchItem) => $"{_scapper.EmagResults(searchItem)}";
 
-        [Route("api/EvomagResults/{searchItem}")]
+        #region !Working
+        //[Route("api/EvomagResults/{searchItem}")]
+
+        //[HttpGet]
+
+        //public string ReadEvomagAds(string searchItem) => $"{_scapper.EvomagResults(searchItem)}";
+
+        //[Route("api/ITarenaResults/{searchItem}")]
+
+        //[HttpGet]
+
+        //public string ITarenaResults(string searchItem) => $"{_scapper.ITArenaResults(searchItem)}";
+
+        //[Route("api/VexioResults/{searchItem}")]
+
+        //[HttpGet]
+
+        //public string VexioAds(string searchItem) => $"{_scapper.VexioResults(searchItem)}";
+
+        //[Route("api/VexioResults/{searchItem}")]
+
+        //[HttpGet]
+
+        //public string IPon(string searchItem) => $"{_scapper.IPonResults(searchItem)}";
+
+        #endregion
+
+        [Route("api/ForITResults/{searchItem}")]
 
         [HttpGet]
 
-        public string ReadEvomagAds(string searchItem) => $"{_scapper.EvomagResults(searchItem)}";
+        public string ForITAds(string searchItem) => $"{_scapper.ForITResults(searchItem)}";
+
+        [Route("api/PcOneResults/{searchItem}")]
+
+        [HttpGet]
+
+        public string PcOneAds(string searchItem) => $"{_scapper.PcOneResults(searchItem)}";
+
+        [Route("api/ItGalaxyResults/{searchItem}")]
+
+        [HttpGet]
+
+        public string ItGalaxyAds(string searchItem) => $"{_scapper.ItGalaxyResults(searchItem)}";
+
     }
 }
