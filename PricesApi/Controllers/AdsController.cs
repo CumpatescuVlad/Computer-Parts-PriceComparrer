a11f09bbcaaa67f4947dae3c4faae62996f245ca
VLAD <Cumpatescuvlad@yaho.com>
2022-12-20 00:54:39 +0200

    Version V0.7

---
+++ PricesApi/Controllers/AdsController.cs
@@ -0,0 +1,21 @@
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DataScrapper.src;
+namespace DataScrapper.Controllers
+{
+   
+    [ApiController]
+    public class AdsController : ControllerBase
+    {
+        private readonly EmagAdsData emag = new();
+
+        [Route("api/Processor/{searchModel}/{pageCount}")]
+
+        [HttpGet]
+
+        public string EmagAds(string pageCount, string searchModel) =>$"{emag.ReadProcessorData(pageCount,searchModel)}";
+       
+
+    }
+}
---
+++ PricesApi/src/EmagAdsData.cs
@@ -0,0 +1,42 @@
+﻿using HtmlAgilityPack;
+using System.Collections;
+using System.Reflection.Metadata;
+
+namespace DataScrapper.src
+{
+    public class EmagAdsData
+    {
+        private readonly HttpClient client = new();
+        private readonly HtmlDocument document = new();
+
+        public string ReadProcessorData(string pageCount,string searchModel)
+        {
+           
+            var HtmlPage = client.GetStringAsync($"https://www.emag.ro/procesoare/p{pageCount}/c").Result;
+
+            document.LoadHtml(HtmlPage);
+
+            var processorAds = document.DocumentNode.SelectNodes("//div[@class='pad-hrz-xs']/a[@data-zone='title']");
+
+           
+            var processorModels = from processorModel in processorAds
+                                  where processorModel.InnerText.Contains(searchModel)
+                                  select processorModel;
+             
+
+            foreach (var processor in processorModels)
+            {
+                Ads.AdTitle += $"{processor.InnerText}\n{processor.Attributes["href"].Value}\n";
+
+               
+            }
+
+            
+
+            return $"{Ads.AdTitle}{Ads.AdPrice}";
+        }
+
+
+
+    }
+}
